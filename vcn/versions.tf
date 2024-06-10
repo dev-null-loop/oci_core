dde9cf3524212161949805f8dfed7b9b62def29a
devnull-loop <dev.null.loop@gmail.com>
2024-06-10 09:46:11 +0300

    removed backend from vcn mod

--- vcn/versions.tf
+++ vcn/versions.tf
@@ -1,18 +1,4 @@
 terraform {
-  backend "s3" {
-    bucket                      = "tfstates"
-    region                      = "eu-frankfurt-1"
-    key                         = "vcn/terraform.tfstate"
-    endpoints                   = { s3 = "https://frvkqml7frsy.compat.objectstorage.eu-frankfurt-1.oraclecloud.com" }
-    shared_credentials_files    = ["/home/bd/.oci/credentials"]
-    skip_credentials_validation = true
-    skip_metadata_api_check     = true
-    skip_region_validation      = true
-    skip_requesting_account_id  = true
-    skip_s3_checksum            = true
-    use_path_style              = true
-  }
-
   required_providers {
     oci = {
       source  = "oracle/oci"
