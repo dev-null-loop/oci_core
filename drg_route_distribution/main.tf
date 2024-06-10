66c0d48454d2adf988bf611887abee49295b5398
devnull-loop <dev.null.loop@gmail.com>
2024-06-10 21:49:00 +0300

    typo

--- drg_route_distribution/main.tf
+++ drg_route_distribution/main.tf
@@ -2,6 +2,6 @@ resource "oci_core_drg_route_distribution" "this" {
   distribution_type = var.distribution_type
   drg_id            = var.drg_id
   defined_tags      = var.defined_tags
-  display_name      = var.distribution_display_name
+  display_name      = var.display_name
   freeform_tags     = var.freeform_tags
 }
