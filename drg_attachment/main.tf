08211902ddf72c712d0bd4897954734ed3680d12
devnull-loop <dev.null.loop@gmail.com>
2024-06-11 14:51:19 +0300

    refactored drg_attachment mod

--- drg_attachment/main.tf
+++ drg_attachment/main.tf
@@ -1,6 +1,14 @@
 resource "oci_core_drg_attachment" "this" {
-  display_name  = var.display_name
-  drg_id        = var.drg_id
-  vcn_id        = var.vcn_id
-  freeform_tags = var.freeform_tags
+  drg_id             = var.drg_id
+  defined_tags       = var.defined_tags
+  display_name       = var.display_name
+  drg_route_table_id = var.drg_route_table_id
+  freeform_tags      = var.freeform_tags
+
+  network_details {
+    id             = var.network_details.id
+    type           = var.network_details.type
+    route_table_id = var.network_details.route_table_id
+    vcn_route_type = var.network_details.vcn_route_type
+  }
 }
--- drg_attachment/variables.tf
+++ drg_attachment/variables.tf
@@ -1,20 +1,39 @@
-variable "vcn_id" {
-  description = "The OCID of the default DRG route table to be assigned to DRG attachments of type VCN"
+variable "drg_id" {
+  description = "(Required) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the DRG."
   type        = string
 }
 
-variable "drg_id" {
-  description = "The OCID of the DRG"
-  type        = string
+variable "defined_tags" {
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  type        = map(string)
+  default     = null
 }
 
 variable "display_name" {
-  description = "Name of the dynamic routing gateway"
+  description = "(Optional) (Updatable) A user-friendly name. Does not have to be unique, and it's changeable. Avoid entering confidential information."
   type        = string
+  default     = "attachment"
 }
 
 variable "freeform_tags" {
-  description = "Free form tags applied to organize and list multiple DRGs"
+  description = "(Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
   type        = map(string)
   default     = {}
+}
+
+variable "drg_route_table_id" {
+  description = "(Optional) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the DRG route table that is assigned to this attachment."
+  type        = string
+  default     = null
+}
+
+variable "network_details" {
+  description = "(Optional) (Updatable)"
+  type = object({
+    id             = string
+    type           = string
+    route_table_id = optional(string)
+    vcn_route_type = optional(string)
+  })
+  default = null
 }
