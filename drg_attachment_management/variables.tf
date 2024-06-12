4a776fdfe8b39cbdd1465131801b7eea09bbd17d
devnull-loop <dev.null.loop@gmail.com>
2024-06-12 13:12:59 +0300

    added network_details block to drg_attachment_management

--- drg_attachment_management/variables.tf
+++ drg_attachment_management/variables.tf
@@ -43,24 +43,13 @@ variable "freeform_tags" {
   default     = {}
 }
 
-variable "vcn_id" {
-  description = "(Optional) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the VCN."
-  type        = string
-  default     = null
+variable "network_details" {
+  description = "(Optional) (Updatable)"
+  type = object({
+    id                  = string
+    ipsec_connection_id = optional(string)
+    route_table_id      = optional(string)
+    type                = string
+  })
+  default = null
 }
-
-# export_drg_route_distribution_id` (Optional) (Updatable) - The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the export route distribution used to specify how routes in the assigned DRG route table are advertised to the attachment. If this value is null, no routes are advertised through this attachment.
-#   network_details` - (Optional)(Updatable)
-#     * `id` -(Required) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the network attached to the DRG.
-#     * `ipsec_connection_id` - The IPSec connection that contains the attached IPSec tunnel.
-#     * `route_table_id` (Optional)(Updatable)- The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the route table the DRG attachment is using.
-
-#       For information about why you would associate a route table with a DRG attachment, see:
-#         * [Transit Routing: Access to Multiple VCNs in Same Region](https://docs.cloud.oracle.com/iaas/Content/Network/Tasks/transitrouting.htm)
-#         * [Transit Routing: Private Access to Oracle Services](https://docs.cloud.oracle.com/iaas/Content/Network/Tasks/transitroutingoracleservices.htm)
-#     * `type` (Required)- The type can be one of these values: `IPSEC_TUNNEL`, `REMOTE_PEERING_CONNECTION`, `VCN`,`VIRTUAL_CIRCUIT`
-# * `route_table_id`(Optional)(Updatable) - The OCID of the route table the DRG attachment is using.
-
-#   For information about why you would associate a route table with a DRG attachment, see:
-#     * [Transit Routing: Access to Multiple VCNs in Same Region](https://docs.cloud.oracle.com/iaas/Content/Network/Tasks/transitrouting.htm)
-#     * [Transit Routing: Private Access to Oracle Services](https://docs.cloud.oracle.com/iaas/Content/Network/Tasks/transitroutingoracleservices.htm) This field is deprecated. Instead, use the `networkDetails` field to view the [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the attached resource.
