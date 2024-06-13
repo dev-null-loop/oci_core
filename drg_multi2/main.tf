d7808a390120b5946f1db3b800ffa61292804b1b
devnull-loop <dev.null.loop@gmail.com>
2024-06-13 15:32:39 +0300

    added v2 for drg_multi

---
+++ drg_multi2/main.tf
@@ -0,0 +1,15 @@
+resource "oci_core_drg" "this" {
+  for_each       = var.providers
+  compartment_id = var.compartment_id
+  defined_tags   = var.defined_tags
+  display_name   = var.display_name
+  freeform_tags  = var.freeform_tags
+  provider       = each.alias
+}
+# resource "oneview_appliance_time_and_locale" "timelocale2" {
+#   for_each = var.provider_list // List contain provider and its alias
+#   locale      = "en_US.UTF-8"
+#   timezone    = "UTC"
+#   ntp_servers = ["10.50.0.50"]
+#   provider = each.alias
+# }
