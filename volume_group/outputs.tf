e3cd6be5aee9ea2c0db3ff78c593b912393ad85c
devnull-loop <dev.null.loop@gmail.com>
2024-08-21 12:54:13 +0300

    vg:added volume_ids output

--- volume_group/outputs.tf
+++ volume_group/outputs.tf
@@ -7,3 +7,8 @@ output "volume_group_replicas" {
   description = "The list of volume group replicas of this volume group."
   value       = oci_core_volume_group.this.volume_group_replicas
 }
+
+output "volume_ids" {
+  description = "OCIDs for the current volumes in this volume group."
+  value       = oci_core_volume_group.this.volume_ids
+}
