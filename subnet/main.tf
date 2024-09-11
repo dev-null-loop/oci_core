ee10f701eb01958ae6714f9a8eca2b51188b1f57
dev-null-loop <dev.null.loop@gmail.com>
2024-09-11 18:28:27 +0300

    subnet:refactoring

--- subnet/main.tf
+++ subnet/main.tf
@@ -1,11 +1,17 @@
 resource "oci_core_subnet" "this" {
+  availability_domain        = var.availability_domain
   cidr_block                 = var.cidr_block
   compartment_id             = var.compartment_id
-  vcn_id                     = var.vcn_id
+  defined_tags               = var.defined_tags
+  dhcp_options_id            = var.dhcp_options_id
   display_name               = var.display_name
   dns_label                  = var.dns_label
   freeform_tags              = var.freeform_tags
-  prohibit_public_ip_on_vnic = var.is_subnet_private
+  ipv6cidr_block             = var.ipv6cidr_block
+  ipv6cidr_blocks            = var.ipv6cidr_blocks
+  prohibit_internet_ingress  = var.prohibit_internet_ingress
+  prohibit_public_ip_on_vnic = var.prohibit_public_ip_on_vnic
   route_table_id             = var.route_table_id
   security_list_ids          = var.security_list_ids
+  vcn_id                     = var.vcn_id
 }
--- subnet/variables.tf
+++ subnet/variables.tf
@@ -1,42 +1,74 @@
+variable "availability_domain" {
+  description = "(Optional) Controls whether the subnet is regional or specific to an availability domain. Oracle recommends creating regional subnets because they're more flexible and make it easier to implement failover across availability domains. Originally, AD-specific subnets were the only kind available to use."
+  type        = number
+  default     = null
+}
 variable "cidr_block" {
-  description = "The CIDR IP address range of the subnet."
+  description = "(Required) (Updatable) The CIDR IP address range of the subnet."
   type        = string
 }
 
 variable "compartment_id" {
-  description = "The OCID of the compartment to contain the subnet."
+  description = "(Required) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment to contain the subnet."
   type        = string
 }
 
-variable "vcn_id" {
-  description = "The OCID of the VCN the service gateway is attached to."
+variable "defined_tags" {
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  type        = map(string)
+  default     = null
+}
+
+variable "dhcp_options_id" {
+  description = "(Optional) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the set of DHCP options the subnet will use. If you don't provide a value, the subnet uses the VCN's default set of DHCP options."
   type        = string
+  default     = null
 }
 
 variable "display_name" {
   description = "(Optional) (Updatable) A user-friendly name. Does not have to be unique, and it's changeable. Avoid entering confidential information."
   type        = string
+  default     = null
 }
 
 variable "dns_label" {
-  description = "A DNS label for the subnet, used in conjunction with the VNIC's hostname and VCN's DNS label to form a fully qualified domain name (FQDN) for each VNIC within this subnet"
+  description = "(Optional) A DNS label for the subnet, used in conjunction with the VNIC's hostname and VCN's DNS label to form a fully qualified domain name (FQDN) for each VNIC within this subnet (for example, `bminstance1.subnet123.vcn1.oraclevcn.com`). Must be an alphanumeric string that begins with a letter and is unique within the VCN. The value cannot be changed."
   type        = string
+  default     = null
 }
 
 variable "freeform_tags" {
-  description = "Free form tags applied to organize and list multiple subnets."
+  description = "(Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
   type        = map(string)
   default     = {}
 }
 
-variable "is_subnet_private" {
-  description = "(Optional) Whether VNICs within this subnet can have public IP addresses. Defaults to false, which means VNICs created in this subnet will automatically be assigned public IP addresses unless specified otherwise during instance launch or VNIC creation (with the `assignPublicIp` flag in [CreateVnicDetails](https://docs.cloud.oracle.com/iaas/api/#/en/iaas/latest/CreateVnicDetails/)). If `prohibitPublicIpOnVnic` is set to true, VNICs created in this subnet cannot have public IP addresses (that is, it's a private subnet)."
+variable "ipv6cidr_block" {
+  description = "(Optional) (Updatable) Use this to enable IPv6 addressing for this subnet. The VCN must be enabled for IPv6. You can't change this subnet characteristic later. All subnets are /64 in size. The subnet portion of the IPv6 address is the fourth hextet from the left (1111 in the following example)."
   type        = string
-  default     = true
+  default     = null
+}
+
+variable "ipv6cidr_blocks" {
+  description = "(Optional) (Updatable) The list of all IPv6 prefixes (Oracle allocated IPv6 GUA, ULA or private IPv6 prefixes, BYOIPv6 prefixes) for the subnet."
+  type        = list(string)
+  default     = []
+}
+
+variable "prohibit_internet_ingress" {
+  description = "(Optional) Whether to disallow ingress internet traffic to VNICs within this subnet. Defaults to false."
+  type        = bool
+  default     = false
+}
+
+variable "prohibit_public_ip_on_vnic" {
+  description = "(Optional) Whether VNICs within this subnet can have public IP addresses. Defaults to false, which means VNICs created in this subnet will automatically be assigned public IP addresses unless specified otherwise during instance launch or VNIC creation (with the `assignPublicIp` flag in [CreateVnicDetails](https://docs.cloud.oracle.com/iaas/api/#/en/iaas/latest/CreateVnicDetails/)). If `prohibitPublicIpOnVnic` is set to true, VNICs created in this subnet cannot have public IP addresses (that is, it's a private subnet)."
+  type        = bool
+  default     = false
 }
 
 variable "route_table_id" {
-  description = "(Optional) (Updatable) The OCID of the route table the subnet will use. If you don't provide a value, the subnet uses the VCN's default route table."
+  description = "(Optional) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the route table the subnet will use. If you don't provide a value, the subnet uses the VCN's default route table."
   type        = string
   default     = null
 }
@@ -44,5 +76,10 @@ variable "route_table_id" {
 variable "security_list_ids" {
   description = "(Optional) (Updatable) The OCIDs of the security list or lists the subnet will use. If you don't provide a value, the subnet uses the VCN's default security list. Remember that security lists are associated *with the subnet*, but the rules are applied to the individual VNICs in the subnet."
   type        = list(string)
-  default     = null
+  default     = []
+}
+
+variable "vcn_id" {
+  description = "(Required) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the VCN to contain the subnet."
+  type        = string
 }
