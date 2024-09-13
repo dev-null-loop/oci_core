e654141ee06358bdecbb65e5e7e513e6c3a4e301
dev-null-loop <dev.null.loop@gmail.com>
2024-09-13 20:13:26 +0300

    subnet: added cidr_block output

--- subnet/outputs.tf
+++ subnet/outputs.tf
@@ -2,3 +2,8 @@ output "id" {
   description = "The subnet's Oracle ID"
   value       = oci_core_subnet.this.id
 }
+
+output "cidr_block" {
+  description = "The subnet's CIDR block.  Example: `10.0.1.0/24`"
+  value       = oci_core_subnet.this.cidr_block
+}
