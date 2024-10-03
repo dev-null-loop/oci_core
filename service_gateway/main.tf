dc67da9f545278a03452696f92f4c772770e776e
dev-null-loop <dev.null.loop@gmail.com>
2024-10-03 13:22:01 +0300

    service gateway:this ~> these

--- service_gateway/main.tf
+++ service_gateway/main.tf
@@ -1,4 +1,4 @@
-data "oci_core_services" "this" {}
+data "oci_core_services" "these" {}
 
 resource "oci_core_service_gateway" "this" {
   compartment_id = var.compartment_id
@@ -9,6 +9,6 @@ resource "oci_core_service_gateway" "this" {
   route_table_id = var.route_table_id
 
   services {
-    service_id = data.oci_core_services.this.services[0].id
+    service_id = data.oci_core_services.these.services[0].id
   }
 }
