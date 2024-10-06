8f627c5b193af1532d551ac797685633a693770e
dev-null-loop <dev.null.loop@gmail.com>
2024-10-06 16:30:17 +0300

    instance:add private_ip output

--- instance/outputs.tf
+++ instance/outputs.tf
@@ -7,3 +7,8 @@ output "boot_volume_id" {
   description = "The OCID of the attached boot volume. If the `source_type` is `bootVolume`, this will be the same OCID as the `source_id`."
   value       = oci_core_instance.this.boot_volume_id
 }
+
+output "private_ip" {
+  description = " The private IP address of instance VNIC. To set the private IP address, use the `private_ip` argument in create_vnic_details."
+  value       = oci_core_instance.this.private_ip
+}
