2303e08926e0e26fd1fde242ba32f0ccb27446ff
dev-null-loop <dev.null.loop@gmail.com>
2024-10-06 12:22:08 +0300

    rpc: small refactoring

--- remote_peering_connection/main.tf
+++ remote_peering_connection/main.tf
@@ -2,7 +2,7 @@ resource "oci_core_remote_peering_connection" "requestor" {
   compartment_id   = var.compartment_id
   drg_id           = var.drg_ids["requestor"]
   defined_tags     = var.defined_tags
-  display_name     = "Requestor"
+  display_name     = var.display_name
   freeform_tags    = var.freeform_tags
   peer_id          = oci_core_remote_peering_connection.acceptor.id
   peer_region_name = var.peer_region_name
--- remote_peering_connection/variables.tf
+++ remote_peering_connection/variables.tf
@@ -3,9 +3,10 @@ variable "compartment_id" {
   type        = string
 }
 
-variable "peer_region_name" {
-  description = "(Optional) The name of the region that contains the RPC you want to peer with.  Example: `us-ashburn-1`"
+variable "display_name" {
+  description = "(Optional) (Updatable) A user-friendly name. Does not have to be unique, and it's changeable. Avoid entering confidential information."
   type        = string
+  default     = "Requestor"
 }
 
 variable "drg_ids" {
@@ -23,4 +24,9 @@ variable "freeform_tags" {
   description = "(Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
   type        = map(string)
   default     = {}
+}
+
+variable "peer_region_name" {
+  description = "(Optional) The name of the region that contains the RPC you want to peer with.  Example: `us-ashburn-1`"
+  type        = string
 }
