0383133e6cc568651d7e8bb856ef90b7b3341946
dev-null-loop <dev.null.loop@gmail.com>
2024-10-07 17:16:53 +0300

    nat_gateway: refactoring

--- nat_gateway/variables.tf
+++ nat_gateway/variables.tf
@@ -1,20 +1,45 @@
+variable "block_traffic" {
+  description = "(Optional) (Updatable) Whether the NAT gateway blocks traffic through it. The default is `false`.  Example: `true`"
+  type        = bool
+  default     = false
+}
+
 variable "compartment_id" {
-  description = "The OCID of the compartment to contain the NAT gateway."
+  description = "(Required) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment to contain the NAT gateway."
   type        = string
 }
 
-variable "vcn_id" {
-  description = "The OCID of the VCN the NAT Gateway is attached to."
-  type        = string
+variable "defined_tags" {
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  type        = map(string)
+  default     = null
 }
 
 variable "display_name" {
-  description = "Name of the NAT gateway"
+  description = "(Optional) (Updatable) A user-friendly name. Does not have to be unique, and it's changeable. Avoid entering confidential information."
   type        = string
+  default     = null
 }
 
 variable "freeform_tags" {
-  description = "Free form tags applied to organize and list multiple NAT gateways"
+  description = "(Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
   type        = map(string)
   default     = {}
+}
+
+variable "public_ip_id" {
+  description = "(Optional) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the public IP address associated with the NAT gateway."
+  type        = string
+  default     = null
+}
+
+variable "route_table_id" {
+  description = "(Optional) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the route table used by the NAT gateway."
+  type        = string
+  default     = null
+}
+variable "vcn_id" {
+  description = "(Required) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the VCN the gateway belongs to."
+  type        = string
+  default     = null
 }
