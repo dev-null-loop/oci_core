736816de6360105d50b5074164a50b95233f022e
dev-null-loop <dev.null.loop@gmail.com>
2025-01-26 20:45:11 +0200

    private_ip: add args

--- private_ip/main.tf
+++ private_ip/main.tf
@@ -1,8 +1,10 @@
 resource "oci_core_private_ip" "this" {
+  defined_tags   = var.defined_tags
   display_name   = var.display_name
   freeform_tags  = var.freeform_tags
   hostname_label = var.hostname_label
   ip_address     = var.ip_address
+  route_table_id = var.route_table_id
   vlan_id        = var.vlan_id
   vnic_id        = var.vnic_id
 }
--- private_ip/variables.tf
+++ private_ip/variables.tf
@@ -1,6 +1,13 @@
+variable "defined_tags" {
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  type        = map(string)
+  default     = null
+}
+
 variable "display_name" {
   description = "(Optional) (Updatable) A user-friendly name. Does not have to be unique, and it's changeable. Avoid entering confidential information."
   type        = string
+  default     = null
 }
 
 variable "freeform_tags" {
@@ -12,20 +19,29 @@ variable "freeform_tags" {
 variable "hostname_label" {
   description = " (Optional) (Updatable) The hostname for the private IP. Used for DNS. The value is the hostname portion of the private IP's fully qualified domain name (FQDN) (for example, bminstance1 in FQDN bminstance1.subnet123.vcn1.oraclevcn.com). Must be unique across all VNICs in the subnet and comply with RFC 952 and RFC 1123."
   type        = string
+  default     = null
 }
 
 variable "ip_address" {
-  description = "Optional) A private IP address of your choice. Must be an available IP address within the subnet's CIDR. If you don't specify a value, Oracle automatically assigns a private IP address from the subnet. Example: 10.0.3.3"
+  description = "(Optional) A private IP address of your choice. Must be an available IP address within the subnet's CIDR. If you don't specify a value, Oracle automatically assigns a private IP address from the subnet. Example: 10.0.3.3"
   type        = string
+  default     = null
+}
+
+variable "route_table_id" {
+  description = "(Optional) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the route table the PrivateIp will use."
+  type        = string
+  default     = null
 }
 
 variable "vlan_id" {
   description = "(Optional) Use this attribute only with the Oracle Cloud VMware Solution. The OCID of the VLAN from which the private IP is to be drawn. The IP address, if supplied, must be valid for the given VLAN."
   type        = string
-  default     = ""
+  default     = null
 }
 
 variable "vnic_id" {
   description = "(Optional) (Updatable) The OCID of the VNIC to assign the private IP to. The VNIC and private IP must be in the same subnet."
   type        = string
+  default     = null
 }
