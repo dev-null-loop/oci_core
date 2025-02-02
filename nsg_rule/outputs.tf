10ecbffe23f797c0bc251eb052e2984e8d86638b
dev-null-loop <dev.null.loop@gmail.com>
2025-02-02 18:41:02 +0200

    nsg_rule_v2: fixed output

--- nsg_rule/outputs.tf
+++ nsg_rule/outputs.tf
@@ -1,6 +1,4 @@
-output "ids" {
+output "id" {
   description = "network security group security rule id"
-  value = { for k, v in oci_core_network_security_group_security_rule.this :
-    k => v.id
-  }
+  value       = oci_core_network_security_group_security_rule.this.id
 }
