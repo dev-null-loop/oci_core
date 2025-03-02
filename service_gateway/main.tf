8a9868ba00e72bd83f1557688414de5b5009304b
dev-null-loop <dev.null.loop@gmail.com>
2025-03-02 21:26:43 +0200

    core:service_gateway:refactoring

--- service_gateway/main.tf
+++ service_gateway/main.tf
@@ -2,13 +2,12 @@ data "oci_core_services" "these" {}
 
 resource "oci_core_service_gateway" "this" {
   compartment_id = var.compartment_id
+  services {
+    service_id = data.oci_core_services.these.services[0].id
+  }
   vcn_id         = var.vcn_id
   defined_tags   = var.defined_tags
   display_name   = var.display_name
   freeform_tags  = var.freeform_tags
   route_table_id = var.route_table_id
-
-  services {
-    service_id = data.oci_core_services.these.services[0].id
-  }
 }
