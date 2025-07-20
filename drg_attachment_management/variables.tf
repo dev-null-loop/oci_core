3b9825f696580408488e58f51dcc53a7021d1a43
dev-null-loop <dev.null.loop@gmail.com>
2025-07-20 13:54:16 +0300

    drg_attachemnt_management: uncommented tags

--- drg_attachment_management/variables.tf
+++ drg_attachment_management/variables.tf
@@ -31,17 +31,17 @@ variable "drg_route_table_id" {
   default     = null
 }
 
-# variable "defined_tags" {
-#   description = "(Optional)(Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
-#   type        = map(string)
-#   default     = null
-# }
+variable "defined_tags" {
+  description = "(Optional)(Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  type        = map(string)
+  default     = null
+}
 
-# variable "freeform_tags" {
-#   description = "(Optional)(Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
-#   type        = map(string)
-#   default     = {}
-# }
+variable "freeform_tags" {
+  description = "(Optional)(Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  type        = map(string)
+  default     = {}
+}
 
 variable "network_details" {
   description = "(Optional) (Updatable)"
