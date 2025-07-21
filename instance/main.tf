a1236bbd8bab7532adaded6e22cbff0dbd3a32dc
dev-null-loop <dev.null.loop@gmail.com>
2025-07-21 16:30:18 +0300

    instance: commented merge_type

--- instance/main.tf
+++ instance/main.tf
@@ -31,7 +31,7 @@ data "cloudinit_config" "this" {
 	templatestring(part.value.content, merge(part.value.vars, {})) :
 	null
       )
-      merge_type = "list(append)+dict(no_replace,recurse_list)+str(append)"
+      #merge_type = "list(append)+dict(no_replace,recurse_list)+str(append)"
     }
   }
 }
