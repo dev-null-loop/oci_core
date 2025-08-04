8ffeb5eb7dd0452440f7bdc080346b6c30c0bf69
dev-null-loop <dev.null.loop@gmail.com>
2025-08-04 12:09:08 +0300

    subnet:+vcn_id output

--- subnet/outputs.tf
+++ subnet/outputs.tf
@@ -7,3 +7,7 @@ output "cidr_block" {
   description = "The subnet's CIDR block.  Example: `10.0.1.0/24`"
   value       = oci_core_subnet.this.cidr_block
 }
+
+output "vcn_id" {
+  value = oci_core_subnet.this.vcn_id
+}
