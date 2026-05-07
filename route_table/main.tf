47fb051cbc8865c4a25b864eadf43af65d7832ca
dev-null-loop <dev.null.loop@gmail.com>
2026-05-07 15:59:59 +0300

    refactored route_table to be minimal

--- route_table/main.tf
+++ route_table/main.tf
@@ -1,24 +1,19 @@
-data "oci_core_services" "these" {}
-
 resource "oci_core_route_table" "this" {
   compartment_id = var.compartment_id
   vcn_id         = var.vcn_id
-  defined_tags   = var.defined_tags
   display_name   = var.display_name
+  defined_tags   = var.defined_tags
   freeform_tags  = var.freeform_tags
+
   dynamic "route_rules" {
     for_each = var.route_rules
-    iterator = rr
+    iterator = rule
+
     content {
-      description = rr.value.description
-      destination = (
-	can(regex("(services|objectstorage)", rr.value.destination)) ?
-	[for i in data.oci_core_services.these.services[*].cidr_block : i
-	if can(regex(rr.value.destination, i))][0] :
-	rr.value.destination
-      )
-      destination_type  = rr.value.destination_type
-      network_entity_id = var.network_entity_ids[rr.value.network_entity_name]
+      description       = try(rule.value.description, null)
+      destination       = rule.value.destination
+      destination_type  = rule.value.destination_type
+      network_entity_id = rule.value.network_entity_id
     }
   }
 }
--- route_table/variables.tf
+++ route_table/variables.tf
@@ -17,38 +17,46 @@ variable "display_name" {
 variable "route_rules" {
   description = "(Optional) (Updatable) The collection of rules used for routing destination IPs to network devices."
   type = list(object({
-    network_entity_name = string
-    description         = optional(string)
-    destination         = string
-    destination_type    = string
+    description       = optional(string)
+    destination       = string
+    destination_type  = optional(string, "CIDR_BLOCK")
+    network_entity_id = string
   }))
-
-  # validation {
-  #   condition     = alltrue([for i in var.route_rules : can(regex("^(ig|sg|lpg|drg|ng|pip)_", i.network_entity_name))])
-  #   error_message = "Error: A network_entity_name is prefixed with (ig|sg|lpg|drg|ng|pip)_(.*), where (.*) is the name of the object representing the network entity"
-  # }
-
-  # validation {
-  #   condition = alltrue([for i in var.route_rules :
-  #   can(regex("^(lpg_.*_(requestor|acceptor))$", i.network_entity_name)) if can(regex("^lpg_", i.network_entity_name))])
-  #   error_message = "valid network_entity_names for local peering gateways are lpg_(.*)_(requestor|acceptor), where (.*) is the name of the object representing the lpg"
-  # }
   default = []
-}
 
-variable "network_entity_ids" {
-  description = "(Required) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) for the route rule's target. For information about the type of targets you can specify, see [Route Tables](https://docs.cloud.oracle.com/iaas/Content/Network/Tasks/managingroutetables.htm)."
-  type        = map(string)
+  validation {
+    condition = alltrue([
+      for rule in var.route_rules :
+      contains(["CIDR_BLOCK", "SERVICE_CIDR_BLOCK"], rule.destination_type)
+    ])
+    error_message = "Each route rule destination_type must be CIDR_BLOCK or SERVICE_CIDR_BLOCK."
+  }
+
+  validation {
+    condition = alltrue([
+      for rule in var.route_rules :
+      length(trimspace(rule.destination)) > 0
+    ])
+    error_message = "Each route rule must include a non-empty destination."
+  }
+
+  validation {
+    condition = alltrue([
+      for rule in var.route_rules :
+      length(trimspace(rule.network_entity_id)) > 0
+    ])
+    error_message = "Each route rule must include a non-empty network_entity_id."
+  }
 }
 
 variable "defined_tags" {
-  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Operations.CostCenter\": \"42\"}`"
   type        = map(string)
   default     = null
 }
 
 variable "freeform_tags" {
-  description = "(Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  description = "(Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Department\": \"Finance\"}`"
   type        = map(string)
   default     = {}
 }
