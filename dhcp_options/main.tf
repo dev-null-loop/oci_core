9256f6e044603c2609fa9596cd227a20c3489804
dev-null-loop <dev.null.loop@gmail.com>
2026-05-22 16:59:58 +0300

    Refactor DHCP options module interface

--- dhcp_options/main.tf
+++ dhcp_options/main.tf
@@ -1,19 +1,18 @@
 resource "oci_core_dhcp_options" "this" {
-  compartment_id   = var.compartment_id
+  compartment_id = var.compartment_id
+  dynamic "options" {
+    for_each = var.options
+    iterator = op
+    content {
+      custom_dns_servers  = op.value.custom_dns_servers
+      search_domain_names = op.value.search_domain_names
+      server_type         = op.value.server_type
+      type                = op.value.type
+    }
+  }
   vcn_id           = var.vcn_id
   defined_tags     = var.defined_tags
   display_name     = var.display_name
   domain_name_type = var.domain_name_type
   freeform_tags    = var.freeform_tags
-
-  options {
-    type               = "DomainNameServer"
-    server_type        = "CustomDnsServer"
-    custom_dns_servers = ["192.168.0.2", "192.168.0.11", "192.168.0.19"]
-  }
-
-  options {
-    type                = "SearchDomain"
-    search_domain_names = ["test.com"]
-  }
 }
--- dhcp_options/outputs.tf
+++ dhcp_options/outputs.tf
@@ -2,3 +2,38 @@ output "id" {
   description = "Oracle ID ([OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm)) for the set of DHCP options."
   value       = oci_core_dhcp_options.this.id
 }
+
+output "compartment_id" {
+  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment containing the set of DHCP options."
+  value       = oci_core_dhcp_options.this.compartment_id
+}
+
+output "display_name" {
+  description = "A user-friendly name. Does not have to be unique, and it's changeable. Avoid entering confidential information."
+  value       = oci_core_dhcp_options.this.display_name
+}
+
+output "domain_name_type" {
+  description = "The search domain name type of DHCP options."
+  value       = oci_core_dhcp_options.this.domain_name_type
+}
+
+output "options" {
+  description = "The collection of individual DHCP options."
+  value       = oci_core_dhcp_options.this.options
+}
+
+output "state" {
+  description = "The current state of the set of DHCP options."
+  value       = oci_core_dhcp_options.this.state
+}
+
+output "time_created" {
+  description = "Date and time the set of DHCP options was created, in the format defined by [RFC3339](https://tools.ietf.org/html/rfc3339)."
+  value       = oci_core_dhcp_options.this.time_created
+}
+
+output "vcn_id" {
+  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the VCN the set of DHCP options belongs to."
+  value       = oci_core_dhcp_options.this.vcn_id
+}
