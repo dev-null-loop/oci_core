a67789914b12e00b75044f0c7c7ea30911c040b6
dev-null-loop <dev.null.loop@gmail.com>
2026-05-25 10:27:11 +0300

    Add listing resource version agreement child module

---
+++ listing_resource_version_agreement/variables.tf
@@ -0,0 +1,9 @@
+variable "listing_id" {
+  description = "(Required) The OCID of the listing."
+  type        = string
+}
+
+variable "listing_resource_version" {
+  description = "(Required) Listing Resource Version."
+  type        = string
+}
