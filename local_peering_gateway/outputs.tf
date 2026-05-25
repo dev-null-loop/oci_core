c165c6c0786cf830772ea6c4b8a5cd078bd075ed
dev-null-loop <dev.null.loop@gmail.com>
2026-05-25 10:29:15 +0300

    Refine local peering gateway exception module

--- local_peering_gateway/outputs.tf
+++ local_peering_gateway/outputs.tf
@@ -9,19 +9,39 @@ output "ids" {
 output "requestor" {
   description = "Requestor LPG attributes."
   value = {
-    id             = oci_core_local_peering_gateway.requestor.id
-    vcn_id         = oci_core_local_peering_gateway.requestor.vcn_id
-    display_name   = oci_core_local_peering_gateway.requestor.display_name
-    route_table_id = oci_core_local_peering_gateway.requestor.route_table_id
+    compartment_id               = oci_core_local_peering_gateway.requestor.compartment_id
+    display_name                 = oci_core_local_peering_gateway.requestor.display_name
+    freeform_tags                = oci_core_local_peering_gateway.requestor.freeform_tags
+    id                           = oci_core_local_peering_gateway.requestor.id
+    is_cross_tenancy_peering     = oci_core_local_peering_gateway.requestor.is_cross_tenancy_peering
+    peer_advertised_cidr         = oci_core_local_peering_gateway.requestor.peer_advertised_cidr
+    peer_advertised_cidr_details = oci_core_local_peering_gateway.requestor.peer_advertised_cidr_details
+    peer_id                      = oci_core_local_peering_gateway.requestor.peer_id
+    peering_status               = oci_core_local_peering_gateway.requestor.peering_status
+    peering_status_details       = oci_core_local_peering_gateway.requestor.peering_status_details
+    route_table_id               = oci_core_local_peering_gateway.requestor.route_table_id
+    state                        = oci_core_local_peering_gateway.requestor.state
+    time_created                 = oci_core_local_peering_gateway.requestor.time_created
+    vcn_id                       = oci_core_local_peering_gateway.requestor.vcn_id
   }
 }
 
 output "acceptor" {
   description = "Acceptor LPG attributes."
   value = {
-    id             = oci_core_local_peering_gateway.acceptor.id
-    vcn_id         = oci_core_local_peering_gateway.acceptor.vcn_id
-    display_name   = oci_core_local_peering_gateway.acceptor.display_name
-    route_table_id = oci_core_local_peering_gateway.acceptor.route_table_id
+    compartment_id               = oci_core_local_peering_gateway.acceptor.compartment_id
+    display_name                 = oci_core_local_peering_gateway.acceptor.display_name
+    freeform_tags                = oci_core_local_peering_gateway.acceptor.freeform_tags
+    id                           = oci_core_local_peering_gateway.acceptor.id
+    is_cross_tenancy_peering     = oci_core_local_peering_gateway.acceptor.is_cross_tenancy_peering
+    peer_advertised_cidr         = oci_core_local_peering_gateway.acceptor.peer_advertised_cidr
+    peer_advertised_cidr_details = oci_core_local_peering_gateway.acceptor.peer_advertised_cidr_details
+    peer_id                      = oci_core_local_peering_gateway.acceptor.peer_id
+    peering_status               = oci_core_local_peering_gateway.acceptor.peering_status
+    peering_status_details       = oci_core_local_peering_gateway.acceptor.peering_status_details
+    route_table_id               = oci_core_local_peering_gateway.acceptor.route_table_id
+    state                        = oci_core_local_peering_gateway.acceptor.state
+    time_created                 = oci_core_local_peering_gateway.acceptor.time_created
+    vcn_id                       = oci_core_local_peering_gateway.acceptor.vcn_id
   }
 }
--- local_peering_gateway/variables.tf
+++ local_peering_gateway/variables.tf
@@ -4,23 +4,23 @@ variable "compartment_id" {
 }
 
 variable "requestor" {
-  description = "Configuration for the requestor side of the LPG pair."
+  description = "Configuration for the requestor LPG in the pair."
   type = object({
     vcn_id         = string
-    display_name   = optional(string, "requestor")
+    display_name   = optional(string)
     route_table_id = optional(string)
-    defined_tags   = optional(map(string), null)
+    defined_tags   = optional(map(string))
     freeform_tags  = optional(map(string), {})
   })
 }
 
 variable "acceptor" {
-  description = "Configuration for the acceptor side of the LPG pair."
+  description = "Configuration for the acceptor LPG in the pair."
   type = object({
     vcn_id         = string
-    display_name   = optional(string, "acceptor")
+    display_name   = optional(string)
     route_table_id = optional(string)
-    defined_tags   = optional(map(string), null)
+    defined_tags   = optional(map(string))
     freeform_tags  = optional(map(string), {})
   })
 }
