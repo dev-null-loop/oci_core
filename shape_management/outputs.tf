f2e9a17549dc8d85b6b42e1d4529a281c998f501
dev-null-loop <dev.null.loop@gmail.com>
2026-05-25 10:58:10 +0300

    Refine shape management child module

--- shape_management/outputs.tf
+++ shape_management/outputs.tf
@@ -2,3 +2,13 @@ output "id" {
   description = "The image's Oracle ID (OCID)."
   value       = oci_core_shape_management.this.id
 }
+
+output "image_id" {
+  description = "The OCID of the image containing the shape."
+  value       = oci_core_shape_management.this.image_id
+}
+
+output "shape_name" {
+  description = "The compatible Shape for the image."
+  value       = oci_core_shape_management.this.shape_name
+}
