cdca6265c570e4ce5257a6d19b022f9443df026c
dev-null-loop <dev.null.loop@gmail.com>
2026-05-25 10:33:52 +0300

    Refactor private IP child module

--- private_ip/outputs.tf
+++ private_ip/outputs.tf
@@ -1,4 +1,89 @@
 output "id" {
-  description = "The private IP's Oracle ID (OCID)."
+  description = "The private IP's Oracle ID ([OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm))."
   value       = oci_core_private_ip.this.id
+}
+
+output "availability_domain" {
+  description = "The private IP's availability domain. This attribute will be null if this is a *secondary* private IP assigned to a VNIC that is in a *regional* subnet.  Example: `Uocm:PHX-AD-1`"
+  value       = oci_core_private_ip.this.availability_domain
+}
+
+output "cidr_prefix_length" {
+  description = "The secondary IPv4 CIDR prefix length."
+  value       = oci_core_private_ip.this.cidr_prefix_length
+}
+
+output "compartment_id" {
+  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment containing the private IP."
+  value       = oci_core_private_ip.this.compartment_id
+}
+
+output "defined_tags" {
+  description = "Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Operations.CostCenter\": \"42\"}`"
+  value       = oci_core_private_ip.this.defined_tags
+}
+
+output "display_name" {
+  description = "A user-friendly name. Does not have to be unique, and it's changeable. Avoid entering confidential information."
+  value       = oci_core_private_ip.this.display_name
+}
+
+output "freeform_tags" {
+  description = "Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Department\": \"Finance\"}`"
+  value       = oci_core_private_ip.this.freeform_tags
+}
+
+output "hostname_label" {
+  description = "The hostname for the private IP. Used for DNS."
+  value       = oci_core_private_ip.this.hostname_label
+}
+
+output "ip_address" {
+  description = "The private IP address of the `privateIp` object. The address is within the CIDR of the VNIC's subnet."
+  value       = oci_core_private_ip.this.ip_address
+}
+
+output "ip_state" {
+  description = "State of the IP address. If an IP address is assigned to a VNIC it is ASSIGNED, otherwise it is AVAILABLE."
+  value       = oci_core_private_ip.this.ip_state
+}
+
+output "ipv4subnet_cidr_at_creation" {
+  description = "Ipv4 Subnet CIDR specified whn creating the PrivateIP."
+  value       = oci_core_private_ip.this.ipv4subnet_cidr_at_creation
+}
+
+output "is_primary" {
+  description = "Whether this private IP is the primary one on the VNIC. Primary private IPs are unassigned and deleted automatically when the VNIC is terminated.  Example: `true`"
+  value       = oci_core_private_ip.this.is_primary
+}
+
+output "lifetime" {
+  description = "Lifetime of the IP address. There are two types of IPv6 IPs:"
+  value       = oci_core_private_ip.this.lifetime
+}
+
+output "route_table_id" {
+  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the route table the IP address or VNIC will use. For more information, see [Source Based Routing](https://docs.oracle.com/iaas/Content/Network/Tasks/managingroutetables.htm#Overview_of_Routing_for_Your_VCN__source_routing)."
+  value       = oci_core_private_ip.this.route_table_id
+}
+
+output "subnet_id" {
+  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the subnet the VNIC is in."
+  value       = oci_core_private_ip.this.subnet_id
+}
+
+output "time_created" {
+  description = "The date and time the private IP was created, in the format defined by [RFC3339](https://tools.ietf.org/html/rfc3339).  Example: `2016-08-25T21:10:29.600Z`"
+  value       = oci_core_private_ip.this.time_created
+}
+
+output "vlan_id" {
+  description = "Applicable only if the `PrivateIp` object is being used with a VLAN as part of the Oracle Cloud VMware Solution. The `vlanId` is the [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the VLAN."
+  value       = oci_core_private_ip.this.vlan_id
+}
+
+output "vnic_id" {
+  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the VNIC the private IP is assigned to."
+  value       = oci_core_private_ip.this.vnic_id
 }
--- private_ip/variables.tf
+++ private_ip/variables.tf
@@ -1,5 +1,11 @@
+variable "cidr_prefix_length" {
+  description = "(Optional) An optional field that when combined with the ipAddress field, will be used to allocate secondary IPv4 CIDRs. The CIDR range created by this combination must be within the subnet's CIDR  and the CIDR range should not collide with any existing IPv4 address allocation. The VNIC ID specified in the request object should not already been assigned more than the max IPv4 addresses. If you don't specify a value, this option will be ignored.  Example: 18"
+  type        = number
+  default     = null
+}
+
 variable "defined_tags" {
-  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Operations.CostCenter\": \"42\"}`"
   type        = map(string)
   default     = null
 }
@@ -11,49 +17,55 @@ variable "display_name" {
 }
 
 variable "freeform_tags" {
-  description = "Free form tags applied to organize and list multiple DRGs"
+  description = "(Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Department\": \"Finance\"}`"
   type        = map(string)
   default     = {}
 }
 
 variable "hostname_label" {
-  description = " (Optional) (Updatable) The hostname for the private IP. Used for DNS. The value is the hostname portion of the private IP's fully qualified domain name (FQDN) (for example, bminstance1 in FQDN bminstance1.subnet123.vcn1.oraclevcn.com). Must be unique across all VNICs in the subnet and comply with RFC 952 and RFC 1123."
+  description = "(Optional) (Updatable) The hostname for the private IP. Used for DNS. The value is the hostname portion of the private IP's fully qualified domain name (FQDN) (for example, `bminstance1` in FQDN `bminstance1.subnet123.vcn1.oraclevcn.com`). Must be unique across all VNICs in the subnet and comply with [RFC 952](https://tools.ietf.org/html/rfc952) and [RFC 1123](https://tools.ietf.org/html/rfc1123)."
   type        = string
   default     = null
 }
 
 variable "ip_address" {
-  description = "(Optional) A private IP address of your choice. Must be an available IP address within the subnet's CIDR. If you don't specify a value, Oracle automatically assigns a private IP address from the subnet. Example: 10.0.3.3"
+  description = "(Optional) A private IP address of your choice. Must be an available IP address within the subnet's CIDR. If you don't specify a value, Oracle automatically assigns a private IP address from the subnet.  Example: `10.0.3.3`"
   type        = string
   default     = null
 }
 
-variable "route_table_id" {
-  description = "(Optional) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the route table the PrivateIp will use."
-  type        = string
-  default     = null
-}
-
-variable "vlan_id" {
-  description = "(Optional) Use this attribute only with the Oracle Cloud VMware Solution. The OCID of the VLAN from which the private IP is to be drawn. The IP address, if supplied, must be valid for the given VLAN."
-  type        = string
-  default     = null
-}
-
-variable "vnic_id" {
-  description = "(Optional) (Updatable) The OCID of the VNIC to assign the private IP to. The VNIC and private IP must be in the same subnet."
+variable "ipv4subnet_cidr_at_creation" {
+  description = "(Optional) Any one of the IPv4 CIDRs allocated to the subnet."
   type        = string
   default     = null
 }
 
 variable "lifetime" {
-  description = "(Optional) (Updatable) Lifetime of the IP address. There are two types of IPv6 IPs: Ephemeral / Reserved"
+  description = "(Optional) (Updatable) Lifetime of the IP address. There are two types of IPs:"
+  type        = string
+  default     = null
+}
+
+variable "route_table_id" {
+  description = "(Optional) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the route table the IP address or VNIC will use. For more information, see [Source Based Routing](https://docs.oracle.com/iaas/Content/Network/Tasks/managingroutetables.htm#Overview_of_Routing_for_Your_VCN__source_routing)."
   type        = string
   default     = null
 }
 
 variable "subnet_id" {
   description = "(Optional) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the subnet from which the private IP is to be drawn. The IP address, *if supplied*, must be valid for the given subnet."
+  type        = string
+  default     = null
+}
+
+variable "vlan_id" {
+  description = "(Optional) Use this attribute only with the Oracle Cloud VMware Solution."
+  type        = string
+  default     = null
+}
+
+variable "vnic_id" {
+  description = "(Optional) (Updatable) The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the VNIC to assign the private IP to. The VNIC and private IP must be in the same subnet."
   type        = string
   default     = null
 }
