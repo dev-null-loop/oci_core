0b9f84a13071d11f58a90fb047a29e9ca8075481
dev-null-loop <dev.null.loop@gmail.com>
2026-05-25 10:48:44 +0300

    Refine public IP child module

--- public_ip/outputs.tf
+++ public_ip/outputs.tf
@@ -2,3 +2,73 @@ output "id" {
   description = "The public IP's Oracle ID ([OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm))."
   value       = oci_core_public_ip.this.id
 }
+
+output "assigned_entity_id" {
+  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the entity the public IP is assigned to, or in the process of being assigned to."
+  value       = oci_core_public_ip.this.assigned_entity_id
+}
+
+output "assigned_entity_type" {
+  description = "The type of entity the public IP is assigned to, or in the process of being assigned to."
+  value       = oci_core_public_ip.this.assigned_entity_type
+}
+
+output "availability_domain" {
+  description = "The public IP's availability domain. This property is set only for ephemeral public IPs that are assigned to a private IP (that is, when the `scope` of the public IP is set to AVAILABILITY_DOMAIN). The value is the availability domain of the assigned private IP.  Example: `Uocm:PHX-AD-1`"
+  value       = oci_core_public_ip.this.availability_domain
+}
+
+output "compartment_id" {
+  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the compartment containing the public IP. For an ephemeral public IP, this is the compartment of its assigned entity (which can be a private IP or a regional entity such as a NAT gateway). For a reserved public IP that is currently assigned, its compartment can be different from the assigned private IP's."
+  value       = oci_core_public_ip.this.compartment_id
+}
+
+output "defined_tags" {
+  description = "Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Operations.CostCenter\": \"42\"}`"
+  value       = oci_core_public_ip.this.defined_tags
+}
+
+output "display_name" {
+  description = "A user-friendly name. Does not have to be unique, and it's changeable. Avoid entering confidential information."
+  value       = oci_core_public_ip.this.display_name
+}
+
+output "freeform_tags" {
+  description = "Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Department\": \"Finance\"}`"
+  value       = oci_core_public_ip.this.freeform_tags
+}
+
+output "ip_address" {
+  description = "The public IP address of the `publicIp` object.  Example: `203.0.113.2`"
+  value       = oci_core_public_ip.this.ip_address
+}
+
+output "lifetime" {
+  description = "Defines when the public IP is deleted and released back to Oracle's public IP pool."
+  value       = oci_core_public_ip.this.lifetime
+}
+
+output "private_ip_id" {
+  description = "Deprecated. Use `assignedEntityId` instead."
+  value       = oci_core_public_ip.this.private_ip_id
+}
+
+output "public_ip_pool_id" {
+  description = "The [OCID](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/identifiers.htm) of the pool object created in the current tenancy."
+  value       = oci_core_public_ip.this.public_ip_pool_id
+}
+
+output "scope" {
+  description = "Whether the public IP is regional or specific to a particular availability domain."
+  value       = oci_core_public_ip.this.scope
+}
+
+output "state" {
+  description = "The public IP's current state."
+  value       = oci_core_public_ip.this.state
+}
+
+output "time_created" {
+  description = "The date and time the public IP was created, in the format defined by [RFC3339](https://tools.ietf.org/html/rfc3339).  Example: `2016-08-25T21:10:29.600Z`"
+  value       = oci_core_public_ip.this.time_created
+}
--- public_ip/variables.tf
+++ public_ip/variables.tf
@@ -3,8 +3,13 @@ variable "compartment_id" {
   type        = string
 }
 
+variable "lifetime" {
+  description = "(Required) Defines when the public IP is deleted and released back to the Oracle Cloud Infrastructure public IP pool. For more information, see [Public IP Addresses](https://docs.cloud.oracle.com/iaas/Content/Network/Tasks/managingpublicIPs.htm)."
+  type        = string
+}
+
 variable "defined_tags" {
-  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  description = "(Optional) (Updatable) Defined tags for this resource. Each key is predefined and scoped to a namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Operations.CostCenter\": \"42\"}`"
   type        = map(string)
   default     = null
 }
@@ -16,14 +21,9 @@ variable "display_name" {
 }
 
 variable "freeform_tags" {
-  description = "(Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm)."
+  description = "(Optional) (Updatable) Free-form tags for this resource. Each tag is a simple key-value pair with no predefined name, type, or namespace. For more information, see [Resource Tags](https://docs.cloud.oracle.com/iaas/Content/General/Concepts/resourcetags.htm).  Example: `{\"Department\": \"Finance\"}`"
   type        = map(string)
   default     = {}
-}
-
-variable "lifetime" {
-  description = "(Required) Defines when the public IP is deleted and released back to the Oracle Cloud Infrastructure public IP pool. For more information, see [Public IP Addresses](https://docs.cloud.oracle.com/iaas/Content/Network/Tasks/managingpublicIPs.htm)."
-  type        = string
 }
 
 variable "private_ip_id" {
